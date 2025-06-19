9e88194ae5501bc5689bdd053040a0baaddd6099
Vybekid <felobitto@gmail.com>
2025-06-19 22:52:12 +0300

    updated

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-Chant = "Hare Krishna\nHare Krishna\n Krishna Krishna\n Hare Hare\n Hare Rama\n Hare Rama\n Rama Rama\n Hare Hare\n"
-print(Chant)
+Chant = "\n Hare Krishna\n Hare Krishna\n Krishna Krishna\n Hare Hare\n Hare Rama\n Hare Rama\n Rama Rama\n Hare Hare\n"
+print(f'Welcome Sir Felix, lets say the Holy Chant together: {Chant}')
